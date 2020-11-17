3dec2fa4e8757dd6ef7c26e76db34ad234713394
Hojin <hojin_p@dongaeltek.co.kr>
2020-11-17 17:02:30 +0900

    증식, 공 튀기

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const _sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
 window.onload = function()
 {
   //document.getElementById("ball1").addEventListener("click", BodyClick);
@@ -16,28 +18,62 @@ function BodyClick(event)
   document.getElementById("dock").appendChild(div);
 }
 
-function Breed()
+async function Breed(div)
 {
-  Cnt = document.body.getElementsByTagName("div").length;
+  if(div.classList.contains('bounce'))
+  {
+    div.classList.remove('bounce');
+    div.offsetWidth;
+    div.classList.add('bounce');
+  }
+  else
+  {
+    div.classList.add('bounce');
+  }
 
-  document.body.innerHTML += "<div class='Ball' onclick='ChangePosition()'></div>";
-  document.body.innerHTML += "<div class='Ball' onclick='ChangePosition()'></div>";
+  await _sleep(200);
+
+  for(i = 1; i <= 9; i++)
+  {
+    for(j = -1; j <= 1; j+=2)
+    {
+      sName = "ball" + i +""+j;
+
+      if(document.getElementById(sName) != null)
+      {
+        tempEle = document.getElementById(sName)
+        tempEle.classList.remove('bounce');
+        tempEle.offsetWidth;
+        tempEle.classList.add('bounce');
+        document.getElementById(sName).style.backgroundColor = "#" + Math.round(Math.random() * 0xffffff).toString(16);
+      }
+      else
+      {
+        appearBox = document.createElement('div');
+        appearBox.classList.add('container');
+        appearBox.classList.add('appear');
+        appearBox.style.left   = (50 + i*5*j) + "%";
+        appearBox.style.bottom = div.style.bottom;
+        bounceBox = document.createElement('div');
+        bounceBox.classList.add('container');
+        bounceBox.classList.add('bounce');
+        //bounceBox.style.animationDelay = 200*i + "ms";
+        ballBox = document.createElement('div');
+        ballBox.id  = sName;
+        ballBox.classList.add('Ball');
+        ballBox.style.backgroundColor = "#" + Math.round(Math.random() * 0xffffff).toString(16);
+        bounceBox.appendChild(ballBox);
+        appearBox.appendChild(bounceBox);
+        document.getElementById("dock").appendChild(appearBox);  
+      }
+    }
+    await _sleep(200);
+  }
 }
+
+
 
 function btn_Test_Click(msg)
 {
   alert(msg);
-}
-
-function ChangePosition()
-{
-    Breed();
-
-    //element = document.getElementById('ball1');
-
-    //element.classList.remove("bounce");
-
-    //element.offsetWidth = element.offsetWidth;
-
-    //element.classList.add("bounce");
 }
